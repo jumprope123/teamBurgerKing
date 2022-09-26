7f70e0fcf1d8640edd7560ba62ccfc581026ff3a
jumprope123 <jumprope123@naver.com>
2022-09-26 15:32:54 +0900

    메뉴 디테일 화면을 리덕스를 이용해 처리하도록 수정

--- src/pages/menu/NewProducts.js
+++ src/pages/menu/NewProducts.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-import { changeState } from "../../store/Store";
+import { changeShowDetailDataState, changeState } from "../../store/Store";
 import WidthNavBar from "../../components/main/menu/WidthNavBar";
+import MenuNavBar from "../../components/main/menu/MenuNavBar";
+import { Link } from "react-router-dom";
 
 const NewProducts = () => {
   /**
@@ -25,13 +27,114 @@ const NewProducts = () => {
    * true인 경우 Header와 Footer가 존재, 반대의 경우 Main페이지만 나타난다.
    */
   useEffect(() => {
-    dispatch(changeState(false));
+    dispatch(changeState(true));
   }, []);
 
   return (
     <React.Fragment>
       <WidthNavBar WidthNavBarData={widthNavBarData} />
-      신제품
+      <MenuNavBar />
+      {/*메인 화면 구성 - START*/}
+      <div className={"mgt30 mgb30"}>
+        <div className={"row w-full min-w910 gx-0"}>
+          <div className={"col-2"}></div>
+          <div className={"col-8"}>
+            <div className={"row fontBM_MenuName"}>
+              <div className={"col-3 textAlignCenter mgt30"}>
+                <Link
+                  to="/showDetail"
+                  className={"textDecorationNone colorBlack"}
+                  onClick={() => {
+                    dispatch(
+                      changeShowDetailDataState({
+                        title: "골든치즈렐라와퍼",
+                        imageURL:
+                          "/image/main/menu/newProducts/goldencheeselellaWhopper.png",
+                        detail: [
+                          {
+                            imageURL:
+                              "/image/main/menu/specialDiscount/goldencheesepack4.png",
+                            title: "골든치즈렐라 팩4",
+                            description:
+                              "골든치즈렐라와퍼+골든치즈렐라치킨버거+크리미모짜볼5조각+너겟킹4조각+콜라R2",
+                          },
+                        ],
+                        returnLink: "/NewProducts",
+                      })
+                    );
+                  }}
+                >
+                  <div>
+                    <img
+                      src="/image/main/menu/newProducts/goldencheeselellaWhopper.png"
+                      style={{ width: "180px", height: "150px" }}
+                    />
+                    <p className={"mgt10"}>골든치즈렐라와퍼</p>
+                  </div>
+                </Link>
+              </div>
+              <div className={"col-3 textAlignCenter mgt30"}>
+                <Link
+                  to="/showDetail"
+                  className={"textDecorationNone colorBlack"}
+                >
+                  <div>
+                    <img
+                      src="/image/main/menu/newProducts/goldencheeselellaChicken.png"
+                      style={{ width: "180px", height: "150px" }}
+                    />
+                    <p className={"mgt10"}>골든치즈렐라치킨버거</p>
+                  </div>
+                </Link>
+              </div>
+              <div className={"col-3 textAlignCenter mgt30"}>
+                <Link
+                  to="/showDetail"
+                  className={"textDecorationNone colorBlack"}
+                >
+                  <div>
+                    <img
+                      src="/image/main/menu/newProducts/tabascoMonster.png"
+                      style={{ width: "180px", height: "150px" }}
+                    />
+                    <p className={"mgt10"}>타바스코몬스터</p>
+                  </div>
+                </Link>
+              </div>
+              <div className={"col-3 textAlignCenter mgt30"}>
+                <Link
+                  to="/showDetail"
+                  className={"textDecorationNone colorBlack"}
+                >
+                  <div>
+                    <img
+                      src="/image/main/menu/newProducts/tabascoDoubleBeef.png"
+                      style={{ width: "180px", height: "150px" }}
+                    />
+                    <p className={"mgt10"}>타바스코더블비프</p>
+                  </div>
+                </Link>
+              </div>
+              <div className={"col-3 textAlignCenter mgt30"}>
+                <Link
+                  to="/showDetail"
+                  className={"textDecorationNone colorBlack"}
+                >
+                  <div>
+                    <img
+                      src="/image/main/menu/newProducts/tabascoShrimp.png"
+                      style={{ width: "180px", height: "150px" }}
+                    />
+                    <p className={"mgt10"}>타바스코슈림프</p>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className={"col-2"}></div>
+        </div>
+      </div>
+      {/*메인 화면 구성 - END*/}
     </React.Fragment>
   );
 };
--- src/store/Store.js
+++ src/store/Store.js
@@ -13,10 +13,39 @@ let useDefaultHeaderFooter = createSlice({
   },
 });
 
+/**
+ * showDetail.js파일이 열릴때 보내줄 title, imageURL, description, 메뉴목록으로돌아가기 link
+ */
+let showDetailData = createSlice({
+  name: "showDetailData",
+  initialState: {
+    title: "골든치즈렐라 팩1",
+    imageURL:
+      "/image/main/menu/specialDiscount/detail_big_goldencheeselella.png",
+    detail: [
+      {
+        imageURL:
+          "/image/main/menu/specialDiscount/detail_big_goldencheeselella.png",
+        title: "골든치즈렐라 팩1",
+        description:
+          "골든치즈렐라와퍼+골든치즈렐라치킨버거+크리미모짜볼5조각+너겟킹4조각+콜라R2",
+      },
+    ],
+    returnLink: "/SpecialDiscount",
+  },
+  reducers: {
+    changeShowDetailDataState(oldState, newState) {
+      return newState.payload;
+    },
+  },
+});
+
 export let { changeState } = useDefaultHeaderFooter.actions;
+export let { changeShowDetailDataState } = showDetailData.actions;
 
 export default configureStore({
   reducer: {
     useDefaultHeaderFooter: useDefaultHeaderFooter.reducer,
+    showDetailData: showDetailData.reducer,
   },
 });
